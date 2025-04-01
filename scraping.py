319fd5d9176bb57372474df2e4dac897bfe6c46f
giunas <49498593+giunas@users.noreply.github.com>
2025-04-01 21:46:33 +0200

    Update scraping.py

--- scraping.py
+++ scraping.py
@@ -7,22 +7,20 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 
 def scrape_sicilia(inizio, fine):
-    print("CHROME:", os.path.exists("/usr/bin/google-chrome"))
-    print("CHROMEDRIVER:", os.path.exists("/usr/bin/chromedriver"))
     options = Options()
     options.add_argument("--headless")
     options.add_argument("--disable-gpu")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--enable-javascript")
-    options.binary_location = "/usr/bin/google-chrome"
 
-    service = Service("/usr/bin/chromedriver")
+    service = Service(ChromeDriverManager().install())
     browser = webdriver.Chrome(service=service, options=options)
     wait = WebDriverWait(browser, 10)
 
